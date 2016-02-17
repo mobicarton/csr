2d869589d13808c604bfaa65bd6b31d56919c913
Damien Brun <makowildcat@gmail.com>
2016-02-17 17:32:29 -0500

    Create interfaces for callback (result, error and rmsdB)

--- library/src/main/java/mobi/carton/csr/ContinuousSpeechRecognition.java
+++ library/src/main/java/mobi/carton/csr/ContinuousSpeechRecognition.java
@@ -8,10 +8,33 @@ import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 
+import java.util.ArrayList;
+
 
 public class ContinuousSpeechRecognition
         implements
         RecognitionListener {
+
+
+    /*
+    INTERFACES
+    */
+
+
+    public interface OnTextListener {
+        void onTextMatched(ArrayList<String> matchedText);
+
+        void onError(int error);
+    }
+
+
+    public interface OnRmsListener {
+        void onRmsChanged(float rms);
+    }
+
+
+    private OnTextListener mOnTextListener;
+    private OnRmsListener mOnRmsListener;
 
 
     private SpeechRecognizer mSpeechRecognizer;
@@ -26,6 +49,7 @@ public class ContinuousSpeechRecognition
         continuousListening = false;
 
         mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(context);
+        mSpeechRecognizer.setRecognitionListener(this);
 
 
         mRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -34,6 +58,26 @@ public class ContinuousSpeechRecognition
         mRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
         mRecognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5); // doesn't really work ?
     }
+
+
+    /*
+    SETTERS
+     */
+
+
+    public void setOnTextListener(OnTextListener l) {
+        mOnTextListener = l;
+    }
+
+
+    public void setOnRmsListener(OnRmsListener l) {
+        mOnRmsListener = l;
+    }
+
+
+    /*
+    IMPLEMENTS
+     */
 
 
     @Override
@@ -50,7 +94,9 @@ public class ContinuousSpeechRecognition
 
     @Override
     public void onRmsChanged(float rmsdB) {
-
+        if (mOnRmsListener != null) {
+            mOnRmsListener.onRmsChanged(rmsdB);
+        }
     }
 
 
@@ -96,6 +142,11 @@ public class ContinuousSpeechRecognition
 
     @Override
     public void onResults(Bundle results) {
+        if (mOnTextListener != null) {
+
+            ArrayList<String> matchedText = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            mOnTextListener.onTextMatched(matchedText);
+        }
 
         if (continuousListening)
             mSpeechRecognizer.startListening(mRecognizerIntent);
